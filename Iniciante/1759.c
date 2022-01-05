e1cfc0befbd9d57e454086892595101d148d3831
Renato Freitas <renatofreitas.design@gmail.com>
2022-01-05 18:47:51 -0300

    Added: 1759

---
+++ Iniciante/1759.c
@@ -0,0 +1,20 @@
+/*********
+Problem 1759
+    Ho Ho Ho
+By Renato Freitas
+**********/
+#include <stdio.h>
+
+int main() {
+
+    int n, i;
+
+    scanf("%d", &n);
+
+    for(i = 0; i < n-1; i++){
+        printf("Ho ");
+    }
+    printf("Ho!\n");
+    
+    return 0;
+}
