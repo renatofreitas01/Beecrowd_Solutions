2b3e4017ce195333d5910ab2aed501ab4ab7d422
Renato Freitas <renatofreitas.design@gmail.com>
2022-01-07 10:57:51 -0300

    Update 1828.c

--- Iniciante/1828.c
+++ Iniciante/1828.c
@@ -5,43 +5,102 @@ By Renato Freitas
 **********/
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+
+static char *const pedra = "pedra";
+static char *const lagarto = "lagarto";
+static char *const papel = "papel";
+static char *const tesoura = "tesoura";
+
+static char *const spock = "Spock";
+
+void buildResultMatrix(int resultMatrix[5][5]);
+
+void getResultMatrixPosition(int *pLine, int *pColumn);
+
+void printResult(int cont, int r);
+
+void validateMatrixPosition(int line, int column);
+
+int getMatchResult(int m[5][5], const int *line, const int *column);
+
+int getvalue(char name[10]) {
+    if (strcmp(name, lagarto) == 0) {
+        return 0;
+    } else if (strcmp(name, pedra) == 0) {
+        return 1;
+    } else if (strcmp(name, papel) == 0) {
+        return 2;
+    } else if (strcmp(name, tesoura) == 0) {
+        return 3;
+    } else if (strcmp(name, spock) == 0) {
+        return 4;
+    }
+    return -1;
+}
+
 
 int main() {
-    int T;
-    int b, c;
-    char ts[] = "tesoura", pp[] = "papel", pd[] = "pedra", lg[] = "lagarto", so[] = "Spock";
-    char str1[10], str2[10];
-    scanf("%d", &T);
-    for (b = 1; b <= T; b++) {
-        scanf("%s %s", str1, str2);
-        if (0 == strcmp(str1, str2))
-            printf("Caso #%d: De novo!\n", b);
-        else if (0 == strcmp(str1, ts)) {
-            if ((0 == strcmp(str2, pp)) || (0 == strcmp(str2, lg)))
-                printf("Caso #%d: Bazinga!\n", b);
-            else if ((0 == strcmp(str2, pd)) || (0 == strcmp(str2, so)))
-                printf("Caso #%d: Raj trapaceou!\n", b);
-        } else if (0 == strcmp(str1, pp)) {
-            if ((0 == strcmp(str2, pd)) || (0 == strcmp(str2, so)))
-                printf("Caso #%d: Bazinga!\n", b);
-            else if ((0 == strcmp(str2, ts)) || (0 == strcmp(str2, lg)))
-                printf("Caso #%d: Raj trapaceou!\n", b);
-        } else if (0 == strcmp(str1, pd)) {
-            if ((0 == strcmp(str2, ts)) || (0 == strcmp(str2, lg)))
-                printf("Caso #%d: Bazinga!\n", b);
-            else if ((0 == strcmp(str2, pp)) || (0 == strcmp(str2, so)))
-                printf("Caso #%d: Raj trapaceou!\n", b);
-        } else if (0 == strcmp(str1, lg)) {
-            if ((0 == strcmp(str2, pp)) || (0 == strcmp(str2, so)))
-                printf("Caso #%d: Bazinga!\n", b);
-            else if ((0 == strcmp(str2, pd)) || (0 == strcmp(str2, ts)))
-                printf("Caso #%d: Raj trapaceou!\n", b);
-        } else if (0 == strcmp(str1, so)) {
-            if ((0 == strcmp(str2, ts)) || (0 == strcmp(str2, pd)))
-                printf("Caso #%d: Bazinga!\n", b);
-            else if ((0 == strcmp(str2, pp)) || (0 == strcmp(str2, lg)))
-                printf("Caso #%d: Raj trapaceou!\n", b);
-        }
+    int t;
+
+    scanf("%d", &t);
+
+    int m[5][5];
+    buildResultMatrix(m);
+
+    int i;
+    for (i = 1; i <= t; i++) {
+        int line, column;
+        getResultMatrixPosition(&line, &column);
+        validateMatrixPosition(line, column);
+
+        int r;
+        r = getMatchResult(m, &line, &column);
+        printResult(i, r);
     }
     return 0;
+}
+
+int getMatchResult(int m[5][5], const int *line, const int *column) {
+    int r;
+    r = m[*line][*column];
+    return r;
+}
+
+void validateMatrixPosition(int line, int column) {
+    if (line == -1 || column == -1) {
+        printf("Unable to read value inputted");
+        exit(-1);
+    }
+}
+
+void printResult(int cont, int r) {
+    switch (r) {
+        case -1:
+            printf("Caso #%d: Bazinga!\n", cont);
+            break;
+        case 0:
+            printf("Caso #%d: De novo!\n", cont);
+            break;
+        case 1:
+            printf("Caso #%d: Raj trapaceou!\n", cont);
+            break;
+    }
+}
+
+void getResultMatrixPosition(int *pLine, int *pColumn) {
+    char str1[10], str2[10];
+
+    scanf("%s %s", str1, str2);
+    (*pLine) = getvalue(str1);
+    (*pColumn) = getvalue(str2);
+}
+
+void buildResultMatrix(int resultMatrix[5][5]) {
+    resultMatrix[0][0] = 0, resultMatrix[0][1] = 1, resultMatrix[0][2] = -1, resultMatrix[0][3] = 1, resultMatrix[0][4] = -1;
+    resultMatrix[1][0] = -1, resultMatrix[1][1] = 0, resultMatrix[1][2] = 1, resultMatrix[1][3] = -1, resultMatrix[1][4] = 1;
+    resultMatrix[2][0] = 1, resultMatrix[2][1] = -1, resultMatrix[2][2] = 0, resultMatrix[2][3] = 1, resultMatrix[2][4] = -1;
+    resultMatrix[3][0] = -1, resultMatrix[3][1] = 1, resultMatrix[3][2] = -1, resultMatrix[3][3] = 0, resultMatrix[3][4] = 1;
+    resultMatrix[4][0] = 1, resultMatrix[4][1] = -1, resultMatrix[4][2] = 1, resultMatrix[4][3] = -1, resultMatrix[4][4] = 0;
+}
 }
